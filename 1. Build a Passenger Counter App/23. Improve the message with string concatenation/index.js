b9d867a50277d53f1d0a3475d73defe92b76cb95
Daniel Buttig <daniel.buttig@gmail.com>
2025-09-29 22:42:30 -0500

    Lesson 23 - More string concat
    Continues covering string concatenation. Had to improve the html file to
    encode utf-8 to be able to see the emoji character properly.

--- 1. Build a Passenger Counter App/23. Improve the message with string concatenation/index.js
+++ 1. Build a Passenger Counter App/23. Improve the message with string concatenation/index.js
@@ -1,6 +1,6 @@
 let welcomeEl = document.getElementById("welcome-el")
 
-let name = "Per Harald Borgen"
+let name = "Daniel"
 let greeting = "Welcome back "
 
 welcomeEl.innerText = greeting + name
@@ -9,3 +9,4 @@ welcomeEl.innerText = greeting + name
 // WRITE YOUR CODE BELOW HERE
 // HINT: count = count + 1
 
+welcomeEl.innerText += "👋"
