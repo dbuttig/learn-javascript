f5e71dcc15563368874c129c12a7c0f6a2402a69
Daniel Buttig <daniel.buttig@gmail.com>
2025-12-31 17:37:06 -0600

    Practice onclick and functions to make a calculator

--- 2. Practice time - part 1/06. Calculator challenge/index.js
+++ 2. Practice time - part 1/06. Calculator challenge/index.js
@@ -7,7 +7,27 @@ document.getElementById("num2-el").textContent = num2
 // Call the correct function when the user clicks on one of the buttons
 // Perform the given calculation using num1 and num2
 // Render the result of the calculation in the paragraph with id="sum-el"
+let sumEl = document.getElementById("sum-el")
 
+function add() {
+  let sum = num1 + num2
+  sumEl.textContent = "Sum: " + sum
+}
+
+function subtract() {
+  let difference = num1 - num2
+  sumEl.textContent = "Sum: " + difference
+}
+
+function divide() {
+  let quotient = num1 / num2
+  sumEl.textContent = "Sum: " + quotient
+}
+
+function multiply() {
+  let product = num1 * num2
+  sumEl.textContent = "Sum: " + product
+}
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
 
