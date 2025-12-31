1c9b423a6bd183e438f742d3f00da120e82be7e9
Daniel Buttig <daniel.buttig@gmail.com>
2025-12-31 15:12:59 -0600

    Practice Rendering Error Messages

--- 2. Practice time - part 1/05. Rendering an error message/index.js
+++ 2. Practice time - part 1/05. Rendering an error message/index.js
@@ -2,4 +2,9 @@
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
 
+let errorMessage = "Something went wrong, please try again"
 
+function purchase() {
+  console.log("Purchase button clicked")
+  document.getElementById("error").innerText = errorMessage
+}
